20b73940b60e75f8ffdd405edd4480e4d3378bf3
viqas <viqas@nakedsoul.org>
2015-09-27 15:52:49 -0400

    homework

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function answers Assignment 2, it will set, get, and calcuate the inverse of a function
+## 
 
-## Write a short comment describing this function
+## purpose of the function is to create a matrix that will hold a couple of variables
 
 makeCacheMatrix <- function(x = matrix()) {
+  m<- NULL
 
+  set < - function(y){
+    x<<-y
+    m<<-NULL
+    
+  }
+    
+  setsolution <- function (inverse) m <<-inverse
+
+  get <- function() x
+  
+  getsolution <- function () m
+  
+list(set = set, get = get, setsolution = setsolution, getsolution = getsolution)
+  
 }
 
 
-## Write a short comment describing this function
+## Solve the inverse of the matrix 
 
 cacheSolve <- function(x, ...) {
+   m<- x$getsolution()
+  
+    if(!is.null(m)){
+     
+     message("woo! dont have to do this again, retrieving cached data")
+     return(m)
+   }
+   
+   data <- x$get()
+   m<-solve(data)
+   x$setsolution(m) 
+   m
         ## Return a matrix that is the inverse of 'x'
 }
